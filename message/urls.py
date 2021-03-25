4982443413768f82717aa8c65ad9b87a59d667d6
Ishan Tiwari <poet.infamous@gmail.com>
2021-03-25 14:16:25 +0530

    Adedd about us

--- message/urls.py
+++ message/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from .views import home_view, dashboard_view, bookclass_view, addcomment_view
 urlpatterns =[
     path('', home_view, name="home"),
     path('dashboard/', dashboard_view),
     path('bookclass/', bookclass_view),
-    path('comment/', addcomment_view)
+    path('comment/', addcomment_view),
+    path('aboutus/', TemplateView.as_view(template_name='aboutus.html'))
 ]
